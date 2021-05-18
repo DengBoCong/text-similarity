44567e27fb13caf4e421ed8eb21a43550d3b68e0
DengBoCong <1210212670@qq.com>
2021-05-18 10:31:31 +0800

    更新欧氏距离、余弦相似度计算

--- sentence2vec/similarity.py
+++ sentence2vec/similarity.py
@@ -18,7 +18,7 @@ def weighted_average(emb1, emb2):
 
     :param emb1: shape = [counts, feature]
     :param emb2: shape = [counts, feature]
-    :return:
+    :return: 相似度分数，shape = [counts,]
     """
     inn = (emb1 * emb2).sum(axis=1)
     emb1norm = np.sqrt((emb1 * emb1).sum(axis=1))
@@ -35,7 +35,22 @@ def weighted_average(emb1, emb2):
 def euclidean(emb1, emb2):
     """ 计算特征向量间的欧氏距离
 
-    :param emb1:
-    :param emb2:
-    :return:
+    :param emb1: shape = [counts, feature]
+    :param emb2: shape = [counts, feature]
+    :return: 欧式距离度量，shape = [counts,]
     """
+    dist = np.sqrt(np.square(emb1 - emb2).sum())
+
+    return dist
+
+
+def cosine(emb1, emb2):
+    """ 计算特征向量间的余弦相似度
+
+    :param emb1: shape = [counts, feature]
+    :param emb2: shape = [counts, feature]
+    :return: 欧式距离度量，shape = [counts,]
+    """
+    cos = (float(np.dot(emb1, emb2)) / (np.linalg.norm(emb1) * np.linalg.norm(emb2)))
+    
+    return (1 - cos) / 2.0
