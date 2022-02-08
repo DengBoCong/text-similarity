87a05c4b6929946f71f68b9ee9ee3bdec0810413
DengBoCong <1210212670@qq.com>
2022-02-08 23:00:17 +0800

    更新一下tokenizer

--- sim/tools/tokenizer.py
+++ sim/tools/tokenizer.py
@@ -11,12 +11,15 @@ from __future__ import print_function
 
 import json
 import math
-
+import unicodedata
 import numpy as np
 import os
+import re
+import sentencepiece as spm
 from collections import defaultdict
 from collections import OrderedDict
 from typing import Any
+from typing import NoReturn
 
 
 class Tokenizer(object):
@@ -488,3 +491,479 @@ class Segment(object):
             return split.join(self.seg.run(sentence))
         elif self.model == "pkuseg":
             return split.join(self.seg.cut(sentence))
+
+
+def truncate_sequences(max_len: int, indices: Any, *sequences) -> list:
+    """截断总长度至不超过max_len
+    :param max_len: 最大长度
+    :param indices: int/list
+    :param sequences: 序列list
+    """
+    sequences = [s for s in sequences if s]
+    if not isinstance(indices, (list, tuple)):
+        indices = [indices] * len(sequences)
+
+    while True:
+        lengths = [len(s) for s in sequences]
+        if sum(lengths) > max_len:
+            i = np.argmax(lengths)
+            sequences[i].pop(indices[i])
+        else:
+            return sequences
+
+
+class BertTokenizerBase(object):
+    """Bert分词器基类
+    """
+
+    def __init__(self,
+                 token_start: Any = "[CLS]",
+                 token_end: Any = "[SEP]",
+                 pre_tokenize: Any = None,
+                 token_translate: dict = None):
+        """
+        :param token_start: 起始token
+        :param token_end: 结束token
+        :param pre_tokenize: 外部传入的分词函数，用作对文本进行预分词。如果传入
+                            pre_tokenize，则先执行pre_tokenize(text)，然后在它
+                            的基础上执行原本的tokenize函数
+        :param token_translate: 映射字典，主要用在tokenize之后，将某些特殊的token替换为对应的token
+        """
+        self._token_pad = "[PAD]"
+        self._token_unk = "[UNK]"
+        self._token_mask = "[MASK]"
+        self._token_start = token_start
+        self._token_end = token_end
+        self._pre_tokenize = pre_tokenize
+        self._token_translate = token_translate or {}
+        self._token_translate_inv = {v: k for k, v in self._token_translate.items()}
+
+    def tokenize(self, text: str, max_len: int = None) -> list:
+        """分词
+        :param text: 切词文本
+        :param max_len: 填充长度
+        """
+        tokens = [self._token_translate.get(token) or token for token in self._tokenize(text)]
+
+        if self._token_start is not None:
+            tokens.insert(0, self._token_start)
+        if self._token_end is not None:
+            tokens.append(self._token_end)
+
+        if max_len is not None:
+            index = int(self._token_end is not None) + 1
+            truncate_sequences(max_len, -index, tokens)
+
+        return tokens
+
+    def token_to_id(self, token: str) -> Any:
+        """token转换为对应的id
+        :param token: token
+        """
+        raise NotImplementedError
+
+    def tokens_to_ids(self, tokens: list) -> list:
+        """token序列转换为对应的id序列
+        :param tokens: token list
+        """
+        return [self.token_to_id(token) for token in tokens]
+
+    def encode(self,
+               first_text: Any,
+               second_text: Any = None,
+               max_len: int = None,
+               pattern: str = "S*E*E",
+               truncate_from: Any = "post") -> tuple:
+        """输出文本对应token id和segment id
+        :param first_text: str/list
+        :param second_text: str/list
+        :param max_len: 最大长度
+        :param pattern: pattern
+        :param truncate_from: 填充位置，str/int
+        """
+        if isinstance(first_text, str):
+            first_tokens = self.tokenize(first_text)
+        else:
+            first_tokens = first_text
+
+        if second_text is None:
+            second_tokens = None
+        elif isinstance(second_text, str):
+            second_tokens = self.tokenize(second_text)
+        else:
+            second_tokens = second_text
+
+        if max_len is not None:
+            if truncate_from == "post":
+                index = -int(self._token_end is not None) - 1
+            elif truncate_from == "pre":
+                index = int(self._token_start is not None)
+            else:
+                index = truncate_from
+
+            if second_text is not None and pattern == "S*E*E":
+                max_len += 1
+            truncate_sequences(max_len, index, first_tokens, second_tokens)
+
+        first_token_ids = self.tokens_to_ids(first_tokens)
+        first_segment_ids = [0] * len(first_token_ids)
+
+        if second_text is not None:
+            if pattern == "S*E*E":
+                idx = int(bool(self._token_start))
+                second_tokens = second_tokens[idx:]
+            second_token_ids = self.tokens_to_ids(second_tokens)
+            second_segment_ids = [1] * len(second_token_ids)
+            first_token_ids.extend(second_token_ids)
+            first_segment_ids.extend(second_segment_ids)
+
+        return first_token_ids, first_segment_ids
+
+    def id_to_token(self, i: int) -> Any:
+        """id转为对应个token
+        :param i: id
+        """
+        raise NotImplementedError
+
+    def ids_to_tokens(self, ids: list) -> list:
+        """id序列转为对应的token序列
+        :param ids: id list
+        """
+        return [self.id_to_token(i) for i in ids]
+
+    def decode(self, ids: list) -> Any:
+        """转为可读文本
+        :param ids: id list
+        """
+        raise NotImplementedError
+
+    def _tokenize(self, text: str) -> list:
+        """
+        :param text: 切词文本
+        """
+        raise NotImplementedError
+
+
+class BertTokenizer(BertTokenizerBase):
+    """Bert原生分词器
+    """
+
+    def __init__(self,
+                 token_dict: Any,
+                 do_lower_case: bool = False,
+                 word_max_len: int = 200,
+                 **kwargs):
+        """
+        :param token_dict: 映射字典或其文件路径
+        :param do_lower_case: 小写化
+        :param word_max_len: 最大长度
+        """
+        super(BertTokenizer, self).__init__(**kwargs)
+        if isinstance(token_dict, str):
+            token_dict = self.load_vocab(token_dict)
+
+        self._do_lower_case = do_lower_case
+        self._token_dict = token_dict
+        self._token_dict_inv = {v: k for k, v in token_dict.items()}
+        self._vocab_size = len(token_dict)
+        self._word_max_len = word_max_len
+
+        for token in ["pad", "unk", "mask", "start", "end"]:
+            try:
+                _token_id = token_dict[getattr(self, f"_token_{token}")]
+                setattr(self, f"_token_{token}_id", _token_id)
+            except Exception as e:
+                print(str(e))
+
+    def token_to_id(self, token: str) -> Any:
+        """token转换为对应的id
+        """
+        return self._token_dict.get(token, self._token_unk_id)
+
+    def id_to_token(self, i: int) -> Any:
+        """id转换为对应的token
+        """
+        return self._token_dict_inv[i]
+
+    def decode(self, ids: list, tokens: list = None) -> Any:
+        """转为可读文本
+        """
+        tokens = tokens or self.ids_to_tokens(ids)
+        tokens = [token for token in tokens if not self.is_special(token)]
+
+        text, flag = "", False
+        for i, token in enumerate(tokens):
+            if token[:2] == "##":
+                text += token[2:]
+            elif len(token) == 1 and self.is_cjk_character(token):
+                text += token
+            elif len(token) == 1 and self.is_punctuation(token):
+                text += token
+                text += " "
+            elif i > 0 and self.is_cjk_character(text[-1]):
+                text += token
+            else:
+                text += " "
+                text += token
+
+        text = re.sub(" +", " ", text)
+        text = re.sub("' (re|m|s|t|ve|d|ll) ", "'\\1 ", text)
+        punctuation = self.cjk_punctuation() + "+-/={(<["
+        punctuation_regex = "|".join([re.escape(p) for p in punctuation])
+        punctuation_regex = "(%s) " % punctuation_regex
+        text = re.sub(punctuation_regex, "\\1", text)
+        text = re.sub("(\d\.) (\d)", "\\1\\2", text)
+
+        return text.strip()
+
+    def _tokenize(self, text: str, pre_tokenize: bool = True) -> list:
+        """基本分词函数
+        """
+        if self._do_lower_case:
+            text = BertTokenizer.lowercase_and_normalize(text)
+
+        if pre_tokenize and self._pre_tokenize is not None:
+            tokens = []
+            for token in self._pre_tokenize(text):
+                if token in self._token_dict:
+                    tokens.append(token)
+                else:
+                    tokens.extend(self._tokenize(token, False))
+
+            return tokens
+
+        spaced = ""
+        for ch in text:
+            if self.is_punctuation(ch) or self.is_cjk_character(ch):
+                spaced += " " + ch + " "
+            elif self.is_space(ch):
+                spaced += " "
+            elif ord(ch) == 0 or ord(ch) == 0xfffd or self.is_control(ch):
+                continue
+            else:
+                spaced += ch
+
+        tokens = []
+        for word in spaced.strip().split():
+            tokens.extend(self.word_piece_tokenize(word))
+
+        return tokens
+
+    def word_piece_tokenize(self, word: str):
+        """word内分成subword
+        """
+        if len(word) > self._word_max_len:
+            return [word]
+
+        tokens, start, end = [], 0, 0
+        while start < len(word):
+            end = len(word)
+            while end > start:
+                sub = word[start:end]
+                if start > 0:
+                    sub = "##" + sub
+                if sub in self._token_dict:
+                    break
+                end -= 1
+
+            if start == end:
+                return [word]
+            else:
+                tokens.append(sub)
+                start = end
+
+        return tokens
+
+    @staticmethod
+    def lowercase_and_normalize(text: str):
+        """转小写，并进行简单的标准化
+        """
+        text = text.lower()
+        text = unicodedata.normalize("NFD", text)
+        text = "".join([ch for ch in text if unicodedata.category(ch) != "Mn"])
+        return text
+
+    @staticmethod
+    def stem(token: str) -> str:
+        """获取token的“词干”（如果是##开头，则自动去掉##）
+        """
+        if token[:2] == "##":
+            return token[2:]
+        else:
+            return token
+
+    @staticmethod
+    def is_space(ch) -> bool:
+        """空格类字符判断
+        """
+        return ch == " " or ch == "\n" or ch == "\r" or ch == "\t" or unicodedata.category(ch) == "Zs"
+
+    @staticmethod
+    def is_punctuation(ch) -> bool:
+        """标点符号类字符判断（全/半角均在此内）
+        提醒：unicodedata.category这个函数在py2和py3下的
+        表现可能不一样，比如u'§'字符，在py2下的结果为'So'，
+        在py3下的结果是'Po'
+        """
+        code = ord(ch)
+        return 33 <= code <= 47 or \
+               58 <= code <= 64 or \
+               91 <= code <= 96 or \
+               123 <= code <= 126 or \
+               unicodedata.category(ch).startswith("P")
+
+    @staticmethod
+    def cjk_punctuation() -> str:
+        return u"\uff02\uff03\uff04\uff05\uff06\uff07\uff08\uff09\uff0a\uff0b\uff0c\uff0d\uff0f\uff1a\uff1b\uff1c\uff1d\uff1e\uff20\uff3b\uff3c\uff3d\uff3e\uff3f\uff40\uff5b\uff5c\uff5d\uff5e\uff5f\uff60\uff62\uff63\uff64\u3000\u3001\u3003\u3008\u3009\u300a\u300b\u300c\u300d\u300e\u300f\u3010\u3011\u3014\u3015\u3016\u3017\u3018\u3019\u301a\u301b\u301c\u301d\u301e\u301f\u3030\u303e\u303f\u2013\u2014\u2018\u2019\u201b\u201c\u201d\u201e\u201f\u2026\u2027\ufe4f\ufe51\ufe54\u00b7\uff01\uff1f\uff61\u3002"
+
+    @staticmethod
+    def is_cjk_character(ch) -> bool:
+        """CJK类字符判断（包括中文字符也在此列）
+        参考：https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+        """
+        code = ord(ch)
+        return 0x4E00 <= code <= 0x9FFF or \
+               0x3400 <= code <= 0x4DBF or \
+               0x20000 <= code <= 0x2A6DF or \
+               0x2A700 <= code <= 0x2B73F or \
+               0x2B740 <= code <= 0x2B81F or \
+               0x2B820 <= code <= 0x2CEAF or \
+               0xF900 <= code <= 0xFAFF or \
+               0x2F800 <= code <= 0x2FA1F
+
+    @staticmethod
+    def is_control(ch: Any) -> bool:
+        """控制类字符判断
+        """
+        return unicodedata.category(ch) in ('Cc', 'Cf')
+
+    @staticmethod
+    def is_special(ch: Any) -> bool:
+        """判断是不是有特殊含义的符号
+        """
+        return bool(ch) and (ch[0] == '[') and (ch[-1] == ']')
+
+    @staticmethod
+    def is_redundant(token: str) -> bool:
+        """判断该token是否冗余（默认情况下不可能分出来）
+        """
+        if len(token) > 1:
+            for ch in BertTokenizer.stem(token):
+                if BertTokenizer.is_cjk_character(ch) or BertTokenizer.is_punctuation(ch):
+                    return True
+
+    def rematch(self, text: str, tokens):
+        """给出原始的text和tokenize后的tokens的映射关系
+        """
+        if self._do_lower_case:
+            text = text.lower()
+
+    @staticmethod
+    def load_vocab(dict_path: str,
+                   encoding: str = "utf-8",
+                   simplified: bool = False,
+                   startswith: list = None) -> Any:
+        """从bert的词典文件中读取词典
+        :param dict_path: 字典文件路径
+        :param encoding: 编码格式
+        :param simplified: 是否过滤冗余部分token
+        :param startswith: 附加在起始的list
+        """
+        token_dict = {}
+        with open(dict_path, "r", encoding=encoding) as reader:
+            for line in reader:
+                token = line.split()
+                token = token[0] if token else line.strip()
+                token_dict[token] = len(token_dict)
+
+        # 是否过滤冗余部分token
+        if simplified:
+            new_token_dict, keep_tokens = {}, []
+            startswith = startswith or []
+            for t in startswith:
+                new_token_dict[t] = len(new_token_dict)
+                keep_tokens.append(token_dict[t])
+
+            for t, _ in sorted(token_dict.items(), key=lambda s: s[1]):
+                if t not in new_token_dict and not BertTokenizer.is_redundant(t):
+                    new_token_dict[t] = len(new_token_dict)
+                    keep_tokens.append(token_dict[t])
+
+            return new_token_dict, keep_tokens
+        else:
+            return token_dict
+
+    @staticmethod
+    def save_vocab(dict_path: str, token_dict: dict, encoding: str = "utf-8") -> NoReturn:
+        """将词典（比如精简过的）保存为文件
+        """
+        with open(dict_path, "w+", encoding=encoding) as writer:
+            for k, v in sorted(token_dict.items(), key=lambda s: s[1]):
+                writer.write(k + "\n")
+
+
+class SpTokenizer(BertTokenizerBase):
+    """基于SentencePiece模型的封装，使用上跟BERTTokenizer基本一致
+    """
+
+    def __init__(self, sp_model_path: str, **kwargs):
+        """
+        :param sp_model_path: SentencePiece模型模型路径
+        """
+        super(SpTokenizer, self).__init__(**kwargs)
+        self.sp_model = spm.SentencePieceProcessor()
+        self.sp_model.Load(sp_model_path)
+        self._token_pad = self.sp_model.IdToPiece(self.sp_model.pad_id())
+        self._token_unk = self.sp_model.IdToPiece(self.sp_model.unk_id())
+        self._vocab_size = self.sp_model.GetPieceSize()
+
+        for token in ["pad", "unk", "mask", "start", "end"]:
+            try:
+                _token = getattr(self, f"_token_{token}")
+                _token_id = self.sp_model.PieceToId(_token)
+                setattr(self, f"_token_{token}_id", _token_id)
+            except Exception as e:
+                print(str(e))
+
+    def token_to_id(self, token: str) -> Any:
+        """token转换为对应的id
+        """
+        return self.sp_model.PieceToId(token)
+
+    def id_to_token(self, i: int) -> Any:
+        """id转换为对应的token
+        """
+        if i < self._vocab_size:
+            return self.sp_model.IdToPiece(i)
+        else:
+            return ""
+
+    def decode(self, ids: list) -> Any:
+        """转为可读文本
+        """
+        tokens = [self._token_translate_inv.get(token) or token for token in self.ids_to_tokens(ids)]
+        text = self.sp_model.DecodePieces(tokens)
+
+        if isinstance(text, bytes):
+            return text.decode(encoding="utf-8", errors="ignore")
+
+        return text
+
+    def _tokenize(self, text: str) -> list:
+        """基本分词函数
+        """
+        if self._pre_tokenize is not None:
+            text = " ".join(self._pre_tokenize(text))
+
+        tokens = self.sp_model.EncodeAsPieces(text)
+        return tokens
+
+    def is_special(self, ch: Any):
+        """判断是不是有特殊含义的符号
+        """
+        return self.sp_model.IsControl(ch) or self.sp_model.IsUnknown(ch) or self.sp_model.IsUnused(ch)
+
+    def is_decodable(self, i: Any):
+        """判断是否应该被解码输出
+        """
+        return (i < self._vocab_size) and not self.is_special(i)
